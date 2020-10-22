6e8a07b6933d4da8f8fb029f8d203e3079f71af1
madhan1115 <madhan.btechy@gmail.com>
2020-10-23 01:58:29 +0530

    first commit

---
+++ src/main/java/com/HelloWorld/maven/MavenApplication.java
@@ -0,0 +1,14 @@
+package com.HelloWorld.maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenApplication.class, args);
+		System.out.println("hello world maddy");
+	}
+
+}
